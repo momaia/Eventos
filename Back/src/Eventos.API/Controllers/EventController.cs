8ebd2597fd6df5998efc89d912cf6c885dc579c2
Mateus Maia <maiamateusotavio@gmail.com>
2023-10-17 22:07:09 -0300

    feat - change the classes name

---
+++ Back/src/Eventos.API/Controllers/EventController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventController : ControllerBase
+    {
+        public EventController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "value";
+        }
+    }
+}
