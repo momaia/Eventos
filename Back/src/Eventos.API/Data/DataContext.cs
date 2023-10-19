db1cc636c4bc97025e9916064a1c51254076cc1c
Mateus Maia <maiamateusotavio@gmail.com>
2023-10-18 22:30:12 -0300

    Configuring database classes

---
+++ Back/src/Eventos.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Eventos.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eventos.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
